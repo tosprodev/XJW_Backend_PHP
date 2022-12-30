50f80cdcc033c34ff370893651c506270ababdee
tosprodev <96832900+tosprodev@users.noreply.github.com>
2022-12-30 11:20:09 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,11 +3,16 @@
  $your_date = strtotime("29-12-2022");
  $datediff =  $your_date - $now;
  $getdays = round($datediff / (60 * 60 * 24));
-if (str_contains($getdays, '-')) {
+/*if (str_contains($getdays, '-')) {
     echo "Expired";
  } else if ($getdays == "0"){
     echo "Last Date";
  } else {
     echo "Expired";
- }
+ }*/
+ $string = 'The lazy fox jumped over the fence';
+
+if (str_contains($string, 'lazy')) {
+    echo "The string 'lazy' was found in the string\n";
+}
  //echo $getdays;
