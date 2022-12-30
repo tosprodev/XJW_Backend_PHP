59f8e040034e613341c3c54b6bdc3fe31c0d60e7
tosprodev <96832900+tosprodev@users.noreply.github.com>
2022-12-30 11:07:10 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php 
  $now = time(); // or your date as well
- $your_date = strtotime("31-12-2022");
+ $your_date = strtotime("31-12-2023");
  $datediff = $now - $your_date;
  
  echo round($datediff / (60 * 60 * 24));
