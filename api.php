d69a34cd625486324b9c1d058975794ee5a8fa90
tosprodev <96832900+tosprodev@users.noreply.github.com>
2022-12-31 02:07:59 +0530

    Update api.php

--- api.php
+++ api.php
@@ -3648,7 +3648,7 @@ if ($result->num_rows > 0) {
 
 						if($_SERVER['REQUEST_METHOD']=='POST'){
  
-							$id = 22;
+							$id = $_POST['id'];
 							$image = $_POST['image'];
 							/*$sql ="SELECT id FROM users ORDER BY id ASC";
 							
