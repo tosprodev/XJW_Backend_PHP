0f7068280bd64733b13833130e32fa85552aa591
tosprodev <96832900+tosprodev@users.noreply.github.com>
2022-12-30 11:30:13 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php 
  $now = time(); // or your date as well
- $your_date = strtotime("30-12-2022");
+ $your_date = strtotime("31-12-2022");
  $datediff =  $now - $your_date;
  $getdays = round($datediff / (60 * 60 * 24));
  
