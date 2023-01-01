daa4f9962f55861774164981279f8c308493e50c
tosprodev <96832900+tosprodev@users.noreply.github.com>
2023-01-01 11:10:06 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,3 @@
 <?php 
- $now = time(); // or your date as well
- $your_date = strtotime("31-12-2022");
- $datediff =  $your_date - $now;
- $getdays = round($datediff / (60 * 60 * 24));
- 
- if ($getdays == -0) {
-    echo "Last Date";
- } else if (strpos($getdays, "-") !== false){
-    echo "Expired";
- } else {
-    echo $getdays;
- }
+echo date("Y-m-d");
  //echo $getdays;
