a5c445df505943da74dd1ebed8afbf01ec3956bb
tosprodev <96832900+tosprodev@users.noreply.github.com>
2023-01-05 08:25:01 +0530

    Update api.php

--- api.php
+++ api.php
@@ -3898,8 +3898,8 @@ if ($result->num_rows > 0) {
 				$page_limit = ceil ($total/$limit); 
 				if($page<=$page_limit){ 
 				$start = ($page - 1) * $limit; 
-				$sql = "SELECT chat.*, users.* FROM chat INNER JOIN user ON chat.uid = user.id WHERE user.uid = $uid limit $start, $limit";
-				//$sql = "SELECT * from chat WHERE uid = '$uid' limit $start, $limit";
+				//$sql = "SELECT chat.*, users.* FROM chat INNER JOIN user ON chat.uid = user.id WHERE user.uid = $uid limit $start, $limit";
+				$sql = "SELECT * from chat WHERE uid = '$uid' limit $start, $limit";
 				$result = mysqli_query($conn,$sql); 
 				$res = array(); 
 				while($row = mysqli_fetch_array($result)){
