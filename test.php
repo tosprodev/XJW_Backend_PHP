b420b7ff955c2f4cffa1b2a3b9648ffa957b833b
tosprodev <96832900+tosprodev@users.noreply.github.com>
2023-01-10 08:47:10 +0530

    Update test.php

--- test.php
+++ test.php
@@ -53,7 +53,7 @@ $sheet = $spreadsheet->getActiveSheet();
 		    array_push($data_from_db, $temp);
 	        }
 //$highestRow = $this->spreadsheet->getActiveSheet()->getHighestRow();
-$data_from_db[$tc]=array("id"=>"","service"=>"","practitioner"=>"","duration"=>"","timeslot"=>"","booking_for"=>"","recipient"=>"","address"=>"","note"=>"","scharge"=>"","tfee"=>"","total"=>"","payment_status"=>"","transaction_id"=>"","invoice_id"=>"","uid"=>"","cur_time"=>"This is Total");
+$data_from_db[5]=array("id"=>"","service"=>"","practitioner"=>"","duration"=>"","timeslot"=>"","booking_for"=>"","recipient"=>"","address"=>"","note"=>"","scharge"=>"","tfee"=>"","total"=>"","payment_status"=>"","transaction_id"=>"","invoice_id"=>"","uid"=>"","cur_time"=>"This is Total");
 
 //set column header
 //set your own column header
